7504de898b5afd10c4af737f013bb60b5be0c952
Kamalesh K <79313198+Kamalesh2006@users.noreply.github.com>
2022-09-04 08:58:29 +0530

    Add files via upload

---
+++ Language Basics/Exercise3.java
@@ -0,0 +1,13 @@
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int sum = 0;
+		for (i=0;i<args.length ;i++) {
+			sum = sum + Integer.parseInt(args[i]);
+		}
+		System.out.println(sum);
+	}
+
+}
